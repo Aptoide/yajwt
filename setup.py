e78155854e3bbea42d332a5b55a0b2832f81e52e
Nuno Nelas <nuno.nelas@aptoide.com>
2021-01-05 12:00:06 +0000

    [yajwt/0.0.8] - Adds missing dependencies to build package

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 """
 # Upload Package: https://pypi.org/project/yajwt/
 python3 setup.py sdist bdist_wheel
-twine upload dist/yajwt-0.0.7*
+twine upload dist/yajwt-0.0.8*
 """
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="yajwt",
-    version="0.0.7",
+    version="0.0.8",
     author="Nuno Nelas",
     author_email="nuno.nelas@icloud.com",
     description="Yet Another JWT wrapper for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/nnelas/yajwt",
+    url="https://github.com/Aptoide/yajwt",
     packages=setuptools.find_packages(),
+    install_requires=required,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
